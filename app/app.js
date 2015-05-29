42462d06c43b0280a9ca840ef852467630d4ff2c
henrisk <henrisk@gmail.com>
2015-05-28 21:05:00 -0300

    Navigation working, and controller able to be instanced ;-)

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
 	'ui.router',
+	'myApp.view1',
+	'myApp.view2',
+	'myApp.user',
 	'myApp.version'
 ])
 .config(function($stateProvider, $urlRouterProvider) {
--- app/sys/user/user.js
+++ app/sys/user/user.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('myApp.user', [])
-.controller('UserController', [function() {
+.controller('UserController', [function($scope) {
 
 }]);
