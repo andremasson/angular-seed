1d0f26248e5a68bd2eca41857f5482c51a08082b
André Masson <masson.andre@gmail.com>
2015-06-25 23:59:23 -0300

    Few changes to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@
 
 module.exports = function (grunt) {
 
+	require('load-grunt-tasks')(grunt);
+
 	var config = {
 		app: 'app',
 		dist: 'dist'
@@ -26,7 +28,7 @@ module.exports = function (grunt) {
 		uglify: {
 			options: {
 				// the banner is inserted at the top of the output
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+				banner: '/*! <%= pkg.name %> - Built <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> */\n'
 			},
 			dist: {
 				files: {
@@ -38,21 +40,46 @@ module.exports = function (grunt) {
 			files: ['test/**/*.html']
 		},
 		jshint: {
-			// define the files to lint
-			files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
 			// configure JSHint (documented at http://www.jshint.com/docs/)
 			options: {
 				  // more options here if you want to override JSHint defaults
-				globals: {
-					jQuery: true,
-					console: true,
-					module: true
+				jshintrc: '.jshintrc',
+				reporter: require('jshint-stylish')
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= config.app %>/{,*/}*.js',
+				'test/spec/{,*/}*.js'
+			]
+		},
+		// Inject Bower components into HTML file
+		wiredep: {
+			app: {
+				ignorePath: /^\/|\.\.\//,
+				src: ['<%= config.app %>/index.html']
+			}
+		},
+		// Rename files for browser caching
+		rev: {
+			dist: {
+				files: {
+					src: [
+						'<%=  %>',
+					]
 				}
 			}
 		},
 		watch: {
-			files: ['<%= jshint.files %>'],
-			tasks: ['jshint', 'qunit' ]
+			livereload: {
+				options: {
+					livereload: '<%= connect.options.livereload %>'
+				},
+				files: [
+					'<%= config.app %>/{,*/}*.html',
+					'.tmp/styles/{,*/}*.css',
+					'<%= config.app %>/images/{,*/}*'
+					]
+			}
 		},
 		connect: {
 			options: {
@@ -77,6 +104,7 @@ module.exports = function (grunt) {
 				options: {
 					open: false,
 					port: 9001,
+					hostname: 'localhost',
 					middleware: function(connect) {
 						return [
 							connect.static('.tmp'),
@@ -91,6 +119,14 @@ module.exports = function (grunt) {
 				options: {
 					base: '<%= config.dist %>',
 					livereload: false
+				}
+			}
+		},
+		mocha: {
+			all: {
+				options: {
+					run: true,
+					urls: ['http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html']
 				}
 			}
 		},
@@ -109,14 +145,41 @@ module.exports = function (grunt) {
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('serve', 'start the server and preview your app, --allow-remote for remote access', function (target) {
+		if (grunt.option('allow-remote')) {
+			grunt.config.set('connect.options.hostname', '0.0.0.0');
+		}
+		if (target === 'dist') {
+			return grunt.task.run(['build', 'connect:dist:keepalive']);
+		}
+
+		grunt.task.run([
+			'clean:server',
+			'wiredep',
+			'concurrent:server',
+			'autoprefixer',
+			'connect:livereload',
+			'watch'
+		]);
+	});
+
+	grunt.registerTask('server', function (target) {
+		grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+		grunt.task.run([target ? ('serve:' + target) : 'serve']);
+	});
 
 	// this would be run by typing "grunt test" on the command line
 	grunt.registerTask('test', ['jshint', 'qunit']);
+
+	grunt.registerTask('build', ['clean', 'concat', 'uglify']);
+
+	grunt.registerTask('clear', ['clean']);
 
 	// the default task can be run just by typing "grunt" on the command line
 	grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
